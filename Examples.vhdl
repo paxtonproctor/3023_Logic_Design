537804a73e3756d5a33d12369e3b3543d95268ab
Paxton Proctor <61135201+paxtonproctor@users.noreply.github.com>
2022-04-08 15:09:12 -0500

    Update Examples.vhdl

--- Examples.vhdl
+++ Examples.vhdl
@@ -49,3 +49,21 @@ U4: OR2  port map (B1, B2, WARNING);
  
 
      end structural;
+ 
+ entity AND2 is
+
+     port (in1, in2: in std_logic;
+
+           out1: out std_logic);
+
+     end AND2;
+
+ 
+
+architecture behavioral_2 of AND2 is
+
+begin
+
+out1 <= in1 and in2;
+
+     end behavioral_2;
